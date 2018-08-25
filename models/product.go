7d0403bce11a16cc1cc2c019d8eb53c60b3bf063
Marko asus E403 <mm.kujala@gmail.com>
2018-08-25 23:30:05 +0300

    comments

--- models/product.go
+++ models/product.go
@@ -9,6 +9,7 @@ import (
 	"github.com/globalsign/mgo/bson"
 )
 
+// Product type includes product fields
 type Product struct {
 	ID          bson.ObjectId `json:"id" bson:"_id"`
 	Name        string        `json:"name" bson:"name"`
@@ -17,6 +18,7 @@ type Product struct {
 	Price       float32       `json:"price" bson:"price"`
 }
 
+// AllProducts from MongoDB
 func AllProducts() ([]Product, error) {
 	prod := []Product{}
 	err := config.Products.Find(bson.M{}).All(&prod)
@@ -26,6 +28,7 @@ func AllProducts() ([]Product, error) {
 	return prod, nil
 }
 
+// PutProduct inserts to MongoDB
 func PutProduct(r *http.Request) (Product, error) {
 	prod := Product{}
 
